d8b14c08e245622684b8ac322fb989e5441a9dbd
inyeongkim <hacerrumbo@gmail.com>
2020-02-24 00:02:28 +0900

    1.0.0

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # my-smart-home
-아두이노를 이용한 자취생의 smart home 만들기
+아두이노를 이용한 자취생의 my smart home 만들기 프로젝트
+
+### Commit 이력
+- 1.0.0
+    - 서브모터 이용 스위쳐 기능 추가
+    - 알람 설정시 LED 알림 기능 추가
+
+### 개발환경
+- Orange Board BLE
+- Arduino 1.8.12
---
+++ mySwitcher/mySwitcher.ino
@@ -0,0 +1,163 @@
+#include <core_build_options.h> // system clock
+#include <swRTC.h>
+
+#include <SoftwareSerial.h> // bluetooth
+#include <Servo.h> // serve motor
+
+swRTC rtc;
+Servo myServo;
+
+int servoLightOnPosition = 180;
+int servoDefaultPosition = 90;
+int servoLightOffPosition = 0;
+int servoPinNum = 9;
+int ledPinNum = 8;
+
+SoftwareSerial BTSerial(4, 5);
+
+int isAlarmOn = 1; // 0 == false, 1 == true
+
+String mobileInputString, nowTime, alarmTime = "190010";
+
+void setup() {
+  rtc.stopRTC();
+  rtc.setTime(23, 59, 00); // hh, mm, ss
+  rtc.setDate(23, 2, 2020); // dd, mm, yyyy
+  rtc.startRTC();
+
+  Serial.begin(9600);
+  BTSerial.begin(9600);
+  Serial.println("ATcommand");  // ATcommand Start
+
+  pinMode(ledPinNum, OUTPUT); // Set LED
+}
+
+void loop() {
+  if (isAlarmOn) {
+    /* alarm on */
+    digitalWrite(ledPinNum, HIGH);
+    nowTime = makeNowTime2String();
+    Serial.println(nowTime);
+    if (!nowTime.compareTo(alarmTime)) {
+      /* Time to wake up */
+      turnOnTheLight();
+      isAlarmOn = 0;
+    }
+  } else {
+    /* alarm off */
+    digitalWrite(ledPinNum, LOW);
+  }
+  
+  /* BlueTooth == async */
+  if (BTSerial.available()) { 
+    mobileInputString = BTSerial.readStringUntil('\n');
+
+    if (mobileInputString == "on") {
+      /* LIGHT ON */
+      turnOnTheLight();
+    } else if (mobileInputString == "off") {
+      /* LIGHT OFF */
+      turnOffTheLight();
+    } else if (mobileInputString == "cancel") {
+      /* CANCELLED */
+      Serial.println("Alarm is cancelled!");
+      isAlarmOn = 0;
+    } else if (mobileInputString.length() == 6) {
+      /* Alarm format must be hhmmss */
+      if (checkValidAlarmTime(mobileInputString)) {
+        Serial.print("Alarm is on ");
+        Serial.println(mobileInputString);
+        alarmTime = mobileInputString;
+        isAlarmOn = 1;
+      }
+    } else {
+      /* Do somthing Default*/
+      Serial.println(mobileInputString);
+    }
+
+  }
+
+  delay(1000); // for test delay
+}
+
+String makeNowTime2String() {
+  String res = "";
+  int h = rtc.getHours();
+  int m = rtc.getMinutes();
+  int s = rtc.getSeconds();
+
+  if (h < 10) {
+    res.concat("0");
+  }
+  res.concat(String(h));
+
+  if (m < 10) {
+    res.concat("0");
+  }
+  res.concat(String(m));
+
+  if (s < 10) {
+    res.concat("0");
+  }
+  res.concat(String(s));
+  return res;
+}
+
+int checkValidAlarmTime(String mobileInputString) {
+  for (int i = 0; i < 6; i++) {
+    if (!(mobileInputString[i] >= '0' && mobileInputString[i] <= '9')) {
+      Serial.print("Wrong Str!");
+      Serial.println(mobileInputString[i]);
+      return 0;
+    }
+  }
+  
+  /* Valid Check */
+  char buf[3];
+  mobileInputString.substring(0,2).toCharArray(buf, 3);
+  int hour = atoi(buf);
+  if (!(0<=hour && hour<=23)){ return 0; }
+    
+  mobileInputString.substring(2,4).toCharArray(buf, 3);
+  int minutes = atoi(buf);
+  if (!(0<=minutes && minutes<=59)){ return 0; }
+  
+  mobileInputString.substring(4,6).toCharArray(buf, 3);
+  int seconds = atoi(buf);
+  if (!(0<=seconds && seconds<=59)){ return 0; }
+  
+  return 1;
+}
+
+void turnOnTheLight() {
+  myServo.attach(servoPinNum);
+  myServo.write(servoLightOnPosition);
+  delay(1000);
+  myServo.write(servoDefaultPosition);
+  delay(1000);
+  myServo.detach();
+}
+
+void turnOffTheLight() {
+  myServo.attach(servoPinNum);
+  myServo.write(servoLightOffPosition);
+  delay(1000);
+  myServo.write(servoDefaultPosition);
+  delay(1000);
+  myServo.detach();
+}
+
+void printNowTime() {
+  Serial.print(rtc.getYear(), DEC);
+  Serial.print("/");
+  Serial.print(rtc.getMonth(), DEC);
+  Serial.print("/");
+  Serial.print(rtc.getDay(), DEC);
+  Serial.print(" ");
+  Serial.print(rtc.getHours(), DEC);
+  Serial.print(":");
+  Serial.print(rtc.getMinutes(), DEC);
+  Serial.print(":");
+  Serial.print(rtc.getSeconds(), DEC);
+  Serial.print("\n");
+}
